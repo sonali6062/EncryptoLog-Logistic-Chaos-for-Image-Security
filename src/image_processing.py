b789ecceb6c9ff372eab404d90d16f9d343f31f0
Sonali kumari <68367399+sonali6062@users.noreply.github.com>
2025-06-18 23:17:57 +0530

    Update image_processing.py

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,1 +1,10 @@
+img=plt.imread("livingroom.jpg")#when loading from a file location
+img=data.astronat()# Loading Built-in Images from skimage.data
+img1 = data.astronaut()
+img2 = data.camera()
+img3 = data.coins()
 
+
+#After loading the image flatten it
+#flat_image=original_image_loaded.flatten()
+flat_img=img.flatten()
